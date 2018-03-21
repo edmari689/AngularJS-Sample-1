b3e56866800752051a8dff8b68e941cb1942043b
Edmari C. Callangan <edmari689@yahoo.com>
2018-03-21 19:55:03 +0900

    Add files via upload

---
+++ AngularJS Sample 1/myCtrl.js
@@ -0,0 +1,8 @@
+app.controller('myCtrl', function($scope) {
+			$scope.name = "";
+			$scope.age = "";
+			$scope.birthplace = "";
+			$scope.country = "";
+			$scope.gender = "";
+			$scope.trait = "";
+			});
